ad71929b1540161058aa9e050d8f5de0c4027751
Janaina Ferreira <83089892+Janaina99@users.noreply.github.com>
2023-09-03 14:18:08 -0300

    Criadas as classes Model e Controller para Visita. Close #99

---
+++ Codigo/AgroVisit/AgroVisitWeb/Models/VisitaModel.cs
@@ -0,0 +1,22 @@
+﻿using Core;
+using System.ComponentModel.DataAnnotations;
+
+namespace AgroVisitWeb.Models
+{
+    public class VisitaModel
+    {
+        public uint Id { get; set; }
+
+        [StringLength(200)]
+        public string? Observacoes { get; set; }
+
+        [Display(Name = "Data e horário:")]
+        [DataType(DataType.Date, ErrorMessage = "É necessário escolher uma data válida.")]
+        [Required(ErrorMessage = "Campo obrigatório")]        
+        public DateTime DataHora { get; set; }
+
+        public string Status { get; set; } = null!;
+
+        public uint IdPropriedade { get; set; }
+    }
+}
