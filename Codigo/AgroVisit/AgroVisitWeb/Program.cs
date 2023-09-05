05469558930d0713d71bade33ab3603940a29ea5
Eduardo Santos <eduardossbispo31@gmail.com>
2023-09-05 15:15:45 -0300

    Atualizações finais. Issue #103

--- Codigo/AgroVisit/AgroVisitWeb/Program.cs
+++ Codigo/AgroVisit/AgroVisitWeb/Program.cs
@@ -7,7 +7,7 @@ namespace AgroVisitWeb
 {
     public class Program
     {
-        public static void Main(String[] args)
+        public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
