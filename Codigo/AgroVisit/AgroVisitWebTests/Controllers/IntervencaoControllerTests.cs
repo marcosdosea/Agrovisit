4af5ade0ade746ef0566fa0423d49acdfa206e24
brunaa-keila <brunakeilaoliveira@gmail.com>
2023-09-21 16:22:11 -0300

    teste

---
+++ Codigo/AgroVisit/AgroVisitWebTests/Controllers/IntervencaoControllerTests.cs
@@ -0,0 +1,205 @@
+﻿using AgroVisitWeb.Mappers;
+using AgroVisitWeb.Models;
+using AutoMapper;
+using Core;
+using Core.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace AgroVisitWeb.Controllers.Tests
+{
+
+    [TestClass()]
+    public class IntervencaoControllerTests
+    {
+        private static IntervencaoController controller;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            // Arrange
+            var mockService = new Mock<IIntervencaoService>();
+            var projeto = new Mock<IProjetoService>();
+
+            IMapper mapper = new MapperConfiguration(cfg =>
+                cfg.AddProfile(new IntervencaoProfile())).CreateMapper();
+
+            mockService.Setup(service => service.GetAll())
+                .Returns(GetTestIntervencoes());
+            mockService.Setup(service => service.Get(1))
+                .Returns(GetTargetIntervencao());
+            mockService.Setup(service => service.Edit(It.IsAny<Intervencao>()))
+                .Verifiable();
+            mockService.Setup(service => service.Create(It.IsAny<Intervencao>()))
+                .Verifiable();
+            controller = new IntervencaoController(mockService.Object, projeto.Object, mapper);
+        }
+
+     
+        [TestMethod()]
+        public void DetailsTest()
+        {
+            // Act
+            var result = controller.Details(1);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(IntervencaoViewModel));
+            IntervencaoViewModel intervencaoModel = (IntervencaoViewModel)viewResult.ViewData.Model;
+            Assert.AreEqual("Pratica 1", intervencaoModel.Pratica);
+            Assert.AreEqual("Descricao da pratica 1", intervencaoModel.Descricao);
+        }
+
+        [TestMethod()]
+        public void CreateTest_Get_Valido()
+        {
+            // Act
+            var result = controller.Create();
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        [TestMethod()]
+        public void CreateTest_Valid()
+        {
+            // Act
+            var result = controller.Create(GetNewIntervencao());
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+        [TestMethod()]
+        public void CreateTest_Post_Invalid()
+        {
+            // Arrange
+            controller.ModelState.AddModelError("Nome", "Campo requerido");
+
+            // Act
+            var result = controller.Create(GetNewIntervencao());
+
+            // Assert
+            Assert.AreEqual(1, controller.ModelState.ErrorCount);
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+
+        [TestMethod()]
+        public void EditTest_Get_Valid()
+        {
+            // Act
+            var result = controller.Edit(1);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(IntervencaoViewModel));
+            IntervencaoViewModel autorModel = (IntervencaoViewModel)viewResult.ViewData.Model;
+            Assert.AreEqual("Pratica 1", autorModel.Pratica);
+            Assert.AreEqual("Descricao da pratica 1", autorModel.Descricao);
+        }
+
+        [TestMethod()]
+        public void EditTest_Post_Valid()
+        {
+            // Act
+            var result = controller.Edit(GetTargetIntervencaoModel().Id, GetTargetIntervencaoModel());
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+
+        [TestMethod()]
+        public void DeleteTest_Post_Valid()
+        {
+            // Act
+            var result = controller.Delete(1);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(IntervencaoViewModel));
+            IntervencaoViewModel intervencaoModel = (IntervencaoViewModel)viewResult.ViewData.Model;
+            Assert.AreEqual("Pratica 1", intervencaoModel.Pratica);
+            Assert.AreEqual("Descricao da pratica 1", intervencaoModel.Descricao);
+        }
+
+        [TestMethod()]
+        public void DeleteTest_Get_Valid()
+        {
+            // Act
+            var result = controller.Delete(GetTargetIntervencaoModel().Id, GetTargetIntervencaoModel());
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+
+        private IntervencaoViewModel GetNewIntervencao()
+        {
+            return new IntervencaoViewModel
+            {
+                Id = 4,
+                Pratica = "Pratica 4",
+                Descricao = "Descricao da pratica 1"
+            };
+
+        }
+        private static Intervencao GetTargetIntervencao()
+        {
+            return new Intervencao
+            {
+                Id = 1,
+                Pratica = "Pratica 1",
+                Descricao = "Descricao da pratica 1"
+            };
+        }
+
+        private IntervencaoViewModel GetTargetIntervencaoModel()
+        {
+            return new IntervencaoViewModel
+            {
+                Id = 2,
+                Pratica = "Pratica 1",
+                Descricao = "Descricao da pratica 1"
+            };
+        }
+
+        private IEnumerable<Intervencao> GetTestIntervencoes()
+        {
+            return new List<Intervencao>
+            {
+                new Intervencao
+                {
+                    Id = 1,
+                    Pratica = "Pratica 2",
+                    Descricao = "Descricao da pratica 2"
+                },
+                new Intervencao
+                {
+                    Id = 2,
+                    Pratica = "Pratica 1",
+                    Descricao = "Descricao da pratica 1"
+                },
+                new Intervencao
+                {
+                    Id = 3,
+                    Pratica = "Pratica 3",
+                    Descricao = "Descricao da pratica 3"
+                },
+            };
+        }
+    }
+}
+    
---
+++ Codigo/AgroVisit/ServiceTests/IntervencaoServiceTests.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core;
+using Core.Service;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.Serialization;
+
+namespace Service.Tests
+{
+    [TestClass()]
+    public class IntervencaoServiceTests
+    {
+        private AgroVisitContext _context;
+        private IIntervencaoService _intervencaoService;
+
+        
+
+
+        [TestInitialize]
+    public void Initialize(DateTime varchar)
+        {
+            //Arrange
+            var builder = new DbContextOptionsBuilder<AgroVisitContext>();
+            builder.UseInMemoryDatabase("AgroVisitDatabase");
+            var options = builder.Options;
+
+            _context = new AgroVisitContext(options);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+            var intervencoes = new List<Intervencao>
+                {
+                    new Intervencao { Id = 1, Pratica = "Pratica 1", Descricao = "Descricao da pratica 1", IdProjeto = 1},
+                    new Intervencao { Id = 2, Pratica = "Pratica 2", Descricao = "Descricao da pratica 2",  IdProjeto = 1 },
+                    new Intervencao { Id = 3, Pratica = "Pratica 3", Descricao = "Descricao da pratica 3",  IdProjeto = 1},
+                };
+
+
+            var projetos = new List<Projeto>
+            {
+                new Projeto { Id = 1, Nome = "Projeto 1", Descricao = "Descricao do projeto 1"},
+                new Projeto { Id = 2, Nome = "Projeto 2", Descricao = "Descricao do projeto 2" },
+                new Projeto { Id = 3, Nome = "Projeto 3", Descricao = "Descricao do projeto 3"},
+            };
+
+
+            _context.AddRange(intervencoes);
+            _context.SaveChanges();
+            _context.AddRange(projetos);
+            _context.SaveChanges();
+
+
+
+            _intervencaoService = new IntervencaoService(_context);
+        }
+
+
+
+
+        [TestMethod()]
+        public void IntervencaoServiceTest()
+        {
+            Assert.Fail();
+        }
+
+
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            // Act
+            _intervencaoService.Create(new Intervencao() { Id = 4, Pratica = "Pratica 4", Descricao = "Descricao da pratica 4", IdProjeto = 2 });
+            // Assert
+            Assert.AreEqual(4, _intervencaoService.GetAll().Count());
+            var intervencao = _intervencaoService.Get(4);
+            Assert.AreEqual("Pratica 4", intervencao.Pratica);
+            Assert.AreEqual("Descricao da pratica 4", intervencao.Descricao);
+            
+
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            // Act
+            _intervencaoService.Delete(2);
+            // Assert
+            Assert.AreEqual(2, _intervencaoService.GetAll().Count());
+            var intervencao = _intervencaoService.Get(2);
+            Assert.AreEqual(null, intervencao);
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            //Act 
+            var intervencao = _intervencaoService.Get(3);
+            intervencao.Pratica = "Pratica 3";
+            intervencao.Descricao = "Descricao da pratica 3";
+            _intervencaoService.Edit(intervencao);
+            //Assert
+            intervencao = _intervencaoService.Get(3);
+            Assert.IsNotNull(intervencao);
+            Assert.AreEqual("Pratica 3 NOVO", intervencao.Pratica);
+            Assert.AreEqual("Descricao da pratica 3 NOVO", intervencao.Descricao);
+        }
+
+        [TestMethod()]
+        public void GetTest()
+        {
+            var intervencao = _intervencaoService.Get(1);
+            Assert.IsNotNull(intervencao);
+            Assert.AreEqual("Pratica 1", intervencao.Pratica);
+            Assert.AreEqual("Descricao da pratica 1", intervencao.Descricao);
+
+        }
+
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            // Act
+            var listaIntervencao = _intervencaoService.GetAll();
+            // Assert
+            Assert.IsInstanceOfType(listaIntervencao, typeof(IEnumerable<Intervencao>));
+            Assert.IsNotNull(listaIntervencao);
+            Assert.AreEqual(3, listaIntervencao.Count());
+            Assert.AreEqual(1, listaIntervencao.First().Id);
+            Assert.AreEqual("Pratica 1", listaIntervencao.First().Pratica);
+        }
+
+        [TestMethod()]
+        public void GetByProjetoTest()
+        {
+            //Act
+            var intervencoes = _intervencaoService.GetByProjeto(1);
+            //Assert
+            Assert.IsNotNull(intervencoes);
+            Assert.AreEqual(3, intervencoes.Count());
+            Assert.AreEqual("Pratica 1", intervencoes.First().Pratica);
+        }
+    }
+}
