dd5c5050eaf74eee0e0890d21fd07747f6d13529
Eduardo Santos <eduardossbispo31@gmail.com>
2023-10-10 14:49:30 -0300

    Implementado API ao projeto. Close #171

---
+++ Codigo/AgroVisit/AgroVisitAPI/Controllers/ClientesController.cs
@@ -0,0 +1,83 @@
+﻿using AutoMapper;
+using Core;
+using Core.Service;
+using Microsoft.AspNetCore.Mvc;
+using AgroVisitAPI.Models;
+
+namespace AgroVisitAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        private readonly IClienteService _clienteService;
+        private readonly IMapper _mapper;
+
+        public ClientesController(IClienteService clienteService, IMapper mapper)
+        {
+            _clienteService = clienteService;
+            _mapper = mapper;
+        }
+
+        // GET: api/<ClientesController>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var listaClientes = _clienteService.GetAll();
+            if (listaClientes == null)
+                return NotFound();
+            return Ok(listaClientes);
+        }
+
+        // GET api/<ClientesController>/5
+        [HttpGet("{id}")]
+        public ActionResult Get(uint id)
+        {
+            Cliente cliente = _clienteService.Get(id);
+            if (cliente == null)
+                return NotFound();
+            return Ok(cliente);
+        }
+
+        // POST api/<ClientesController>
+        [HttpPost]
+        public ActionResult Post([FromBody] ClienteViewModel clienteModel)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Dados inválidos.");
+
+            var cliente = _mapper.Map<Cliente>(clienteModel);
+            _clienteService.Create(cliente);
+
+            return Ok();
+        }
+
+        // PUT api/<ClientesController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] ClienteViewModel clienteModel)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Dados inválidos.");
+
+            var cliente = _mapper.Map<Cliente>(clienteModel);
+            if (cliente == null)
+                return NotFound();
+
+            _clienteService.Edit(cliente);
+
+            return Ok();
+        }
+
+        // DELETE api/<ClientesController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(uint id)
+        {
+            Cliente cliente = _clienteService.Get(id);
+            if (cliente == null)
+                return NotFound();
+
+            _clienteService.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ Codigo/AgroVisit/AgroVisitAPI/Models/ClienteViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgroVisitAPI.Models
+{
+    public class ClienteViewModel
+    {
+        [Key]
+        public uint Id { get; set; }
+
+        [Display(Name = "CPF")]
+        [StringLength(15)]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Cpf { get; set; } = null!;
+
+        [Display(Name = "Nome")]
+        [StringLength(50)]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Nome { get; set; } = null!;
+
+        [Display(Name = "Data de Nascimento")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/mm/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? DataNascimento { get; set; }
+
+        [Display(Name = "Cidade")]
+        [StringLength(20)]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Cidade { get; set; } = null!;
+
+        [Display(Name = "Bairro")]
+        [StringLength(25)]
+        public string? Bairro { get; set; }
+
+        [Display(Name = "Estado")]
+        [StringLength(2)]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Estado { get; set; } = null!;
+
+        [Display(Name = "Rua")]
+        [StringLength(60)]
+        public string? Rua { get; set; }
+
+        [Display(Name = "N° Residência")]
+        public int? NumeroCasa { get; set; }
+
+        [Display(Name = "Telefone")]
+        [StringLength(15)]
+        [RegularExpression(@"^(\([1-9]{2}\)|[1-9]{2}) ?(?:[2-8]|9[1-9])[0-9]{3}\-?[0-9]{4}$", ErrorMessage = "Número inválido")]
+        public string? Telefone { get; set; }
+
+        [Display(Name = "Agrônomo")]
+        [Required]
+        public uint IdEngenheiroAgronomo { get; set; }
+    }
+}
