6fca98351a823c90a4db808bd4a9106ff7f3f92c
Eduardo <eduardossbispo31@gmail.com>
2023-10-14 16:15:08 -0300

    Autorização implementada. Close #170

--- AnaliseProjeto/Dump/agrovisit_aspnetusers.sql
+++ AnaliseProjeto/Dump/agrovisit_aspnetusers.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: agrovisit
 -- ------------------------------------------------------
--- Server version	8.0.30
+-- Server version	5.7.43-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -37,11 +37,11 @@ CREATE TABLE `aspnetusers` (
   `TwoFactorEnabled` tinyint(1) NOT NULL,
   `LockoutEnd` datetime(6) DEFAULT NULL,
   `LockoutEnabled` tinyint(1) NOT NULL,
-  `AccessFailedCount` int NOT NULL,
+  `AccessFailedCount` int(11) NOT NULL,
   PRIMARY KEY (`Id`),
   UNIQUE KEY `UserNameIndex` (`NormalizedUserName`),
   KEY `EmailIndex` (`NormalizedEmail`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -50,6 +50,7 @@ CREATE TABLE `aspnetusers` (
 
 LOCK TABLES `aspnetusers` WRITE;
 /*!40000 ALTER TABLE `aspnetusers` DISABLE KEYS */;
+INSERT INTO `aspnetusers` VALUES ('11817985-1df0-40f2-8559-395639ab92ba','agronomo@gmail.com','AGRONOMO@GMAIL.COM','agronomo@gmail.com','AGRONOMO@GMAIL.COM',0,'AQAAAAEAACcQAAAAECNq8f45nWqLbWYNRuc6Xn3o2rrgGxCG/PTKWDQuEh0x79w6Q3pl5mPl6SNjaECorA==','62HNOD4M6HF2T733DMCMDXYX34ZXXSTT','a8de1b98-7502-4824-b60c-38462b983137',NULL,0,0,NULL,1,0),('85351b0e-ba8f-4e29-80b7-4854c52c4d81','admin@gmail.com','ADMIN@GMAIL.COM','admin@gmail.com','ADMIN@GMAIL.COM',0,'AQAAAAEAACcQAAAAEKaEns6/C54sgcvnLvFsFrboZ/vKm/rPB2cKIQq/Knz3TNM9taY9fsLc6EsoQIElXA==','XQUHTQTYYQV2VFCT53K23MFZXZP3QHM5','6c5b08c9-9ea1-4d9a-be66-2d779a5d3bad',NULL,0,0,NULL,1,0);
 /*!40000 ALTER TABLE `aspnetusers` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -62,4 +63,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-02 12:40:05
+-- Dump completed on 2023-10-14 15:50:32
