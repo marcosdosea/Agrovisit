be9f9963c3b8453cfd3595d715e54a6604b88de7
Janaina Ferreira <83089892+Janaina99@users.noreply.github.com>
2023-10-15 13:58:24 -0300

    API de visita criada. Close #178

---
+++ Codigo/AgroVisit/AgroVisitAPI/Mappers/VisitaProfile.cs
@@ -0,0 +1,14 @@
+﻿using AgroVisitAPI.Models;
+using AutoMapper;
+using Core;
+
+namespace AgroVisitAPI.Mappers
+{
+    public class VisitaProfile : Profile
+    {
+        public VisitaProfile() 
+        {
+            CreateMap<VisitaViewModel, Visita>().ReverseMap();
+        }
+    }
+}
---
+++ Codigo/AgroVisit/AgroVisitAPI/Models/VisitaViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace AgroVisitAPI.Models
+{
+    public class VisitaViewModel
+    {
+        [Key]
+        public uint Id { get; set; }
+
+        [Display(Name = "Observações")]
+        [StringLength(200)]
+        public string? Observacoes { get; set; }
+
+        [Display(Name = "Data e horário")]
+        [DataType(DataType.DateTime, ErrorMessage = "É necessário escolher uma data válida.")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public DateTime DataHora { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Status { get; set; } = null!;
+
+        [Display(Name = "Propriedade")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public uint IdPropriedade { get; set; }
+        [Display(Name = "Propriedade")]
+        public SelectList? ListaPropriedades { get; set; }
+        [Display(Name = "Propriedade")]
+        public string? NomePropriedade { get; set; }
+        [Display(Name = "Cliente")]
+        public string? NomeCliente { get; set; }
+    }
+}
