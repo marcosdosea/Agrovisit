09dcb8e7084bf8d604ca9fc0b7637f8ae4048fe3
EduardoSSB <eduardossbispo31@gmail.com>
2024-11-26 11:31:42 -0300

    Correções realizadas. Resolved #167

--- Codigo/AgroVisit/Core/AgroVisitContext.cs
+++ Codigo/AgroVisit/Core/AgroVisitContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Core;
 
@@ -36,10 +37,6 @@ public partial class AgroVisitContext : DbContext
     public virtual DbSet<Solo> Solos { get; set; }
 
     public virtual DbSet<Visita> Visita { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySQL("server=localhost;port=3306;user=root;password=root;database=AgroVisit");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
